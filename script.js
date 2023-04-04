dad0021f784fd7ae5c93785227c60af5c57e1e17
AhmedOmar0318 <114923747+AhmedOmar0318@users.noreply.github.com>
2023-04-04 14:37:17 +0000

    The updated code now uses async/await syntax to fetch and process data.

--- script.js
+++ script.js
@@ -6,52 +6,50 @@ const numberOfHeroes = 733;
 const SUPERHERO_TOKEN = '3492498610968981';
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`;
 
-const displaySuperhero = (superhero) => {
+const displaySuperHero = async (superhero) => {
+  const url = `${BASE_URL}/${superhero.id}/powerstats`
   const name = `<p>Hero Name: ${superhero.name}</p>`;
-  searchedHero.value = '';
+  searchedHero.value = ''
   heroImageDiv.innerHTML = 
-    `${name}<img src="${superhero.image.url}" alt='' height = 100 width = 100  />`;
-  fetch(`${BASE_URL}/${superhero.id}/powerstats`)
-    .then(response => response.json())
-    .then(powerstats => {    
-      heroImageDiv.innerHTML +=
-        `<p>Powerstats:<br> 
-        Intelligence: ${powerstats.intelligence}<br>
-        Strength: ${powerstats.strength}<br>
-        Speed: ${powerstats.speed}<br>
-        Power: ${powerstats.power}<br>
-        Combat: ${powerstats.combat}<br>
-        Durability: ${powerstats.durability}     
-        </p>`;
-    });
-};
+    `${name}<img src="${superhero.image.url}" alt='' height = 100 width = 100  />` 
+  const response = await fetch(url)
+  const data = await response.json()      
+    heroImageDiv.innerHTML +=
+        `<p>Powerstats<br><br> 
+        Intelligence: ${data.intelligence}<br>
+        Strength: ${data.strength}<br>
+        Speed: ${data.speed}<br>
+        Power: ${data.power}<br>
+        Combat: ${data.combat}<br>
+        Durability: ${data.durability}     
+        </p>`  
+}
 
-const getSuperHeroById = (id) => {
-  fetch(`${BASE_URL}/${id}`)
-    .then(response => response.json())
-    .then(superhero => {
-      displaySuperhero(superhero);
-    });
-};
+const getSuperHeroById = async (id) => {
+  const url = `${BASE_URL}/${id}`
+  const response = await fetch(url)
+    const data = await response.json()    
+      displaySuperHero(data)    
+}
 
-const searchSuperHeroByName = (name) => {
-  fetch(`${BASE_URL}/search/${name}`)
-    .then(response => response.json())
-    .then(json => {
-      const results = json.results;
+const searchSuperHeroByName = async (name) => {
+  const url = `${BASE_URL}/search/${name}`
+  const response = await fetch(url)
+    const data = await response.json()   
+      const results = data.results;
       if (!results || results.length === 0) {    
         heroImageDiv.innerHTML = `<p>Can't find the searched superhero.</p>`;
-        return;
+        return
       }
-      const superhero = results[0]; 
-      displaySuperhero(superhero);
-    });
-};
+      const superhero = results[0] 
+      displaySuperHero(superhero)    
+}
 
 const getRandomSuperHero = () => {
   const randomId = Math.floor(Math.random() * numberOfHeroes) + 1;
-  getSuperHeroById(randomId);
-};
+  getSuperHeroById(randomId)
+}
 
 randomSuperHero.onclick = getRandomSuperHero;
-searchButton.onclick = () => searchSuperHeroByName(searchedHero.value);
+searchButton.onclick = () => searchSuperHeroByName(searchedHero.value)
+
